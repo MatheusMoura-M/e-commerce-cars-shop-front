e49c9f668a1414cafb47d33d9613ef9f4b754d66
Matheus M <matheus150101miranda@gmail.com>
2023-04-06 11:38:23 -0300

    create nav bar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import { Home } from "../pages/home";
 const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
