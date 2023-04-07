48f72f8579c36b2715b851574583f0a80fcbf0c8
Leonardo Neves <101361395+Leo-neves20@users.noreply.github.com>
2023-04-07 12:20:12 -0300

    add: add plus five grays collors and three success colors (#7)

--- src/styles/global.style.ts
+++ src/styles/global.style.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
 
     :root{
+        
         --brand-1: #4529E6;
         --brand-2: #5126EA;
         --brand-3: #B0A6F0;
@@ -14,10 +15,19 @@ const GlobalStyle = createGlobalStyle`
         --grey-3: #868E96;
         --grey-4: #ADB5BD;
         --grey-5: #CED4DA;
+        --grey-6: #DEE2E6;
+        --grey-7: #E9ECEF;
+        --grey-8: #F1F3F5;
+        --grey-9: #F8F9FA;
+        --grey-10: #FDFDFD;
+        --whiteFixed: #FFFFFF;
 
         --alert-1: #CD2B31;
         --alert-2: #FDD8D8;
         --alert-3: #FFE5E5;
+        --sucess-1: #18794E;
+        --sucess-2: #CCEBD7;
+        --sucess-3: #DDF3E4;
 
         --random-1: #E34D8C;
         --random-2: #C04277;
