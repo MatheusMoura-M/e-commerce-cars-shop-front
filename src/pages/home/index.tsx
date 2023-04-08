8c417a0409a1896e67c94d0e915c091ca4da0d15
Matheus M <matheus150101miranda@gmail.com>
2023-04-08 10:28:59 -0300

    input component created

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,5 @@
-import { Box, Button, FormControl, Input, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Header from "../../components/header";
-import { Footer } from "../../components/footer";
 
 export const Home = () => {
   return (
